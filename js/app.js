37231eef31725f0b056e70c37e779c6141e3ca80
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2015-09-17 18:38:35 -0700

    Added New game functionality. When the user clicks on +New Game button, a new game starts. This is a page reload function.

--- js/app.js
+++ js/app.js
@@ -1,17 +1,38 @@
-
 $(document).ready(function(){
+
+	/*var secretNo;
+	var guessNo;
+
+	function secretNum(min, max) {
+		var min = 1,
+			max = 100;
+		var secret = Math.floor(Math.random() * (max - min + 1)) + min;
+		return secret;
+		console.log('secret');
+	}
+
+	secretNum(1, 100);*/
+
 	
 	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+  	$('.what').click(function(){
+    	$('.overlay').fadeIn(1000);
 
   	});
 
   	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
+  	$('a.close').click(function(){
+  		$('.overlay').fadeOut(1000);
   	});
+
+  	$('.new').on('click', function(){
+		newGame();
+	});
 
 });
 
 
+function newGame() {
+	document.location.reload(true);
+	//secretNum();
+}
